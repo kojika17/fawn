7468d180c5b55bf9e03d8164d9144881099b9689
久保　知己 <kubo@maboroshi.biz>
2017-03-16 16:48:18 +0900

    add base files

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp         = require('gulp');
+var autoprefixer = require('autoprefixer');
+var cssMqpacker  = require('css-mqpacker');
+var postcss      = require('gulp-postcss');
+var assets       = require('postcss-assets');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync').create();
+
+/**
+ * Task
+ */
+
+// BrowserSync
+gulp.task('bs', function() {
+  browserSync.init({
+    server: {
+        baseDir: './'
+    }
+  });
+});
+
+
+/**
+ * Build
+ */
+ // CSS
+gulp.task('build:css', function() {
+  return gulp.src('./core/**/*.scss')
+    .pipe(sass())
+    .pipe(postcss([
+      cssMqpacker({ sort: true }),
+      // autoprefixer(),
+      // assets({
+      //   loadPaths: ['static/']
+      // })
+    ]))
+    .pipe(gulp.dest('./test'));
+});
+
+/**
+ * Watch
+ */
+gulp.task('watch', function() {
+  gulp.watch('./core/**/*.scss', ['build:css']);
+  gulp.watch('./test/*.css').on('change', browserSync.reload);
+});
+
+
+/**
+ * Command
+ */
+
+// 開発
+gulp.task('default', ['build:css', 'bs', 'watch']);
